824e8502d9eefde84e1dca8651985015aa61848e
Vishnu Alla <vishnu.alla@wavemaker.com>
2017-07-28 14:02:07 +0000

    sdf Files modified - [LoginService]

--- services/LoginService/src/com/guardian/loginservice/LoginService.java
+++ services/LoginService/src/com/guardian/loginservice/LoginService.java
@@ -1,6 +1,6 @@
 /*Copyright (c) 2016-2017 imaginea.com All Rights Reserved.
  This software is the confidential and proprietary information of imaginea.com You shall not disclose such Confidential Information and shall use it only in accordance
- with the terms of the source code license agreement you entered into with imaginea.com*/
+ with the terms of the  source code license agreement you entered into with imaginea.com*/
 package com.guardian.loginservice;
 
 import javax.servlet.http.HttpServletRequest;
